3db796dfb8230e7665b547df333229509c258815
Jonathan Phan <jonathanphan@berkeley.edu>
2025-06-28 12:13:38 -0700

    Changed hamburger button to be from React-icons

--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import '../styles/Navbar.css';
-import { FaGithub } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa';
+import { FaBars } from 'react-icons/fa';
 
 export default function Navbar({ onToggleSidebar, githubUrl = "#" }) {
   return (
     <nav className="navbar">
       <div className="navbar-left">
         <button className="sidebar-toggle" onClick={onToggleSidebar}>
-          <span className="hamburger-line"></span>
-          <span className="hamburger-line"></span>
-          <span className="hamburger-line"></span>
+          <FaBars size={20} />
         </button>
         <div className="brand-logo">
           <div className="brand-line">CENTER FOR</div>
